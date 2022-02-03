906ad6cda6d7386b773afe2d2d92231dc23907f7
Anya Murphy <anya.murphy@mac.com>
2022-02-03 15:48:06 +0000

    First commit

---
+++ SupportBank/SupportBank/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+string[] transactions = File.ReadAllLines(@"C:\Users\8184rj\source\repos\Support-Bank-C\SupportBank\SupportBank\Transactions2014.csv");
+
+foreach (string transaction in transactions)
+{
+    Console.WriteLine(transaction);
+}
