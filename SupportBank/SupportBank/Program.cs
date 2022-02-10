d32f0a6488380fc8f9474dad4e257696a65014e1
Anya Murphy <anya.murphy@mac.com>
2022-02-10 14:46:25 +0000

    Progess - list, classes, transactions loop

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -1,9 +1,40 @@
 ﻿
-Console.WriteLine("Hello, World!");
+string[] transactionsFileContent = File.ReadAllLines(@"C:\Users\8184rj\source\repos\Support-Bank-C\SupportBank\SupportBank\Transactions2014.csv");
 
-string[] transactions = File.ReadAllLines(@"C:\Users\8184rj\source\repos\Support-Bank-C\SupportBank\SupportBank\Transactions2014.csv");
+transactionsFileContent = transactionsFileContent.Skip(1).ToArray();
 
-foreach (string transaction in transactions)
+object[] transactions = new object[transactionsFileContent.Length];
+
+Console.WriteLine(transactions.Length);
+
+//List<string> transactions = new List<string>();
+
+foreach (string transaction in transactionsFileContent)
 {
-    Console.WriteLine(transaction);
+    string[] tsplit = transaction.Split(',');
+    Transactions eachTransaction = new Transactions();
+    eachTransaction.Date = tsplit[0];
+    eachTransaction.From = tsplit[1];
+    eachTransaction.To = tsplit[2];
+    eachTransaction.Narrative = tsplit[3];
+    eachTransaction.Amount = Convert.ToInt32(tsplit[4]);
+    //transactions.Add(eachTransaction); // <------
+    Console.WriteLine(tsplit[0]);
 }
+
+
+class Account
+{
+    public int Balance { get; set; }
+}
+
+class Transactions
+{
+    public string Date { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Narrative { get; set; }
+    public int Amount { get; set; }
+}
+
+//new Transactions(tsplit[0], tsplit[1]), tsplit[2], tsplit[3], tsplit[4]
